b7b7685442896f3f47b3d780a5bf1c1c8f87fe14
foqc <fabianq44@gmail.com>
2023-11-30 20:41:02 -0500

    adding new ways to do the same

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,103 +1,96 @@
-import { pipe } from 'fp-ts/function'
-import * as O from 'fp-ts/Option'
-import * as A from 'fp-ts/Array'
-import * as NEA from 'fp-ts/NonEmptyArray'
-import { eqString } from 'fp-ts/Eq'
+import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/Option";
+import * as A from "fp-ts/Array";
+import { Eq } from "fp-ts/string";
 
-import * as OM from '.';
+import * as OM from ".";
 
 describe("OM.fromList", () => {
   describe("given empty list", () => {
     it("returns O.none", () => {
-      const result = OM.fromList([])
+      const result = OM.fromList([]);
       expect(result).toEqual(O.none);
-    })
-  })
+    });
+  });
 
   describe("given a list with a single elemnt", () => {
     it("returns O.some(One(...))", () => {
-      const result = OM.fromList([1])
+      const result = OM.fromList([1]);
       expect(
-        result._tag === "Some"
-        && result.value.tag === "One"
-        && result.value.value === 1
+        result._tag === "Some" &&
+          result.value.tag === "One" &&
+          result.value.value === 1
       ).toBeTruthy();
-    })
-  })
+    });
+  });
 
   describe("given a list with a multiple items", () => {
     it("returns O.some(Many(...))", () => {
-      const result = OM.fromList([1, 2, 3])
+      const result = OM.fromList([1, 2, 3]);
       expect(
-        result._tag === "Some"
-        && result.value.tag === "Many"
-        && A.size(result.value.value) === 3
-      ).toBeTruthy()
-    })
-  })
-})
+        result._tag === "Some" &&
+          result.value.tag === "Many" &&
+          A.size(result.value.value) === 3
+      ).toBeTruthy();
+    });
+  });
+});
 
 describe("getEq instance", () => {
-  const exampleOneNE = pipe(["hey", "you"], A.append("!"))
-  const exampleTwoNE = pipe(["how", "I", "wish", "you", "were"], A.append("here"))
-  const eqStringOM = OM.getEq(eqString)
+  const exampleOneNE = pipe(["hey", "you"], A.append("!"));
+  const exampleTwoNE = pipe(
+    ["how", "I", "wish", "you", "were"],
+    A.append("here")
+  );
+
+  const eqStringOM = OM.getEq(Eq);
 
   describe("given the Eq instance of String", () => {
     it("returns True for two One's with equal strings inside", () => {
-      expect(
-        eqStringOM.equals(OM.one("Hello"), OM.one("Hello"))
-      ).toBeTruthy()
-    })
+      expect(eqStringOM.equals(OM.one("Hello"), OM.one("Hello"))).toBeTruthy();
+    });
 
     it("returns False for two One's with no equal strings inside", () => {
-      expect(
-        eqStringOM.equals(OM.one("Hello"), OM.one("World"))
-      ).toBeFalsy()
-    })
+      expect(eqStringOM.equals(OM.one("Hello"), OM.one("World"))).toBeFalsy();
+    });
 
     it("returns True for two Many's with equal strings inside", () => {
       expect(
         eqStringOM.equals(OM.many(exampleOneNE), OM.many(exampleOneNE))
-      ).toBeTruthy()
-    })
+      ).toBeTruthy();
+    });
 
     it("returns False for two Many's with not equal strings inside", () => {
       expect(
         eqStringOM.equals(OM.many(exampleOneNE), OM.many(exampleTwoNE))
-      ).toBeFalsy()
-    })
+      ).toBeFalsy();
+    });
 
     it("returns False if one is one and the other is many", () => {
       expect(
         eqStringOM.equals(OM.one("hey"), OM.many(exampleOneNE))
-      ).toBeFalsy()
-    })
-  })
-})
+      ).toBeFalsy();
+    });
+  });
+});
 
 describe("map", () => {
-  const eqStringOM = OM.getEq(eqString)
+  const eqStringOM = OM.getEq(Eq);
   const f = (x: string) => x + " " + x;
 
   it("applies f to value inside one", () => {
     expect(
-      pipe(
-        OM.one("hello"),
-        OM.map(f),
-        (x) => eqStringOM.equals(x, OM.one("hello hello")),
+      pipe(OM.one("hello"), OM.map(f), (x) =>
+        eqStringOM.equals(x, OM.one("hello hello"))
       )
-    ).toBeTruthy()
-  })
+    ).toBeTruthy();
+  });
 
   it("applies f to value inside many", () => {
-    const actualOM = pipe(["hey", "you"], A.append("!"), OM.many)
-    const expectedOM = pipe(["hey hey", "you you"], A.append("! !"), OM.many)
+    const actualOM = pipe(["hey", "you"], A.append("!"), OM.many);
+    const expectedOM = pipe(["hey hey", "you you"], A.append("! !"), OM.many);
     expect(
-      pipe(
-        actualOM,
-        OM.map(f),
-        (x) => eqStringOM.equals(x, expectedOM),
-      )
-    ).toBeTruthy()
-  })
-})
+      pipe(actualOM, OM.map(f), (x) => eqStringOM.equals(x, expectedOM))
+    ).toBeTruthy();
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,20 @@
-import { pipe } from 'fp-ts/function'
-import * as O from 'fp-ts/Option'
-import * as NEA from 'fp-ts/NonEmptyArray'
-import { Eq } from 'fp-ts/Eq'
-import * as TE from 'fp-ts/TaskEither'
+import { pipe } from "fp-ts/function";
+import * as O from "fp-ts/Option";
+import * as NEA from "fp-ts/NonEmptyArray";
+import { Eq } from "fp-ts/Eq";
+import * as TE from "fp-ts/TaskEither";
 
-import axios from 'axios';
+import axios from "axios";
 
-type OneOrMany<T> = { tag: "One", value: T } | { tag: "Many", value: NEA.NonEmptyArray<T> }
+type OneOrMany<T> =
+  | { tag: "One"; value: T }
+  | { tag: "Many"; value: NEA.NonEmptyArray<T> };
 
-export const one = <T>(x: T): OneOrMany<T> => ({ tag: "One", value: x })
-export const many = <T>(x: NEA.NonEmptyArray<T>): OneOrMany<T> => 
-  ({ tag: "Many", value: x })
+export const one = <T>(x: T): OneOrMany<T> => ({ tag: "One", value: x });
+export const many = <T>(x: NEA.NonEmptyArray<T>): OneOrMany<T> => ({
+  tag: "Many",
+  value: x,
+});
 
 /* Build OneOrMany from a list
     fromList([]) // => O.none
@@ -21,61 +25,101 @@ export const fromList = <T>(x: T[]): O.Option<OneOrMany<T>> => {
   return pipe(
     x,
     NEA.fromArray,
-    O.map(
-      (y) => {
-        if(y.length === 1) {
-          return one(y[0])
-        } else {
-          return many(y)
-        }
-      })
-  )
-}
+    O.map((y) => {
+      if (y.length === 1) {
+        return one(y[0]);
+      } else {
+        return many(y);
+      }
+    })
+  );
+};
+
+//Another way to do the same fromList function above
+export const fromList2 = <T>(x: T[]): O.Option<OneOrMany<T>> => {
+  if (x.length === 0) {
+    return O.none;
+  }
+
+  if (x.length === 1) {
+    return O.some(one(x[0]));
+  }
+
+  const result = O.getOrElse(() => NEA.of(x[0]))(NEA.fromArray(x));
+  return O.some(many(result));
+};
 
 /* Checks equality of OneOrMany */
 export const getEq = <T>(eqInner: Eq<T>): Eq<OneOrMany<T>> => ({
   equals: (x, y) => {
-    if(x.tag === "One" && y.tag === "One") {
-      return eqInner.equals(x.value, y.value)
-    } else if(x.tag === "Many" && y.tag === "Many") {
-      return NEA.getEq(eqInner).equals(x.value, y.value)
+    if (x.tag === "One" && y.tag === "One") {
+      return eqInner.equals(x.value, y.value);
+    } else if (x.tag === "Many" && y.tag === "Many") {
+      return NEA.getEq(eqInner).equals(x.value, y.value);
     }
-    return false
+    return false;
   },
-})
+});
 
 /* Checks equality of OneOrMany */
-export const map = <A, B>(f: (a: A) => B) => (a: OneOrMany<A>): OneOrMany<B> => {
-  switch (a.tag) {
-    case "One":
-      return one(f(a.value))
-    case "Many":
-      return pipe(a.value, NEA.map(f), many)
-  }
-}
+export const map =
+  <A, B>(f: (a: A) => B) =>
+  (a: OneOrMany<A>): OneOrMany<B> => {
+    switch (a.tag) {
+      case "One":
+        return one(f(a.value));
+      case "Many":
+        return pipe(a.value, NEA.map(f), many);
+    }
+  };
+
+//Another way to do the same map function above
+export const map2 =
+  <A, B>(f: (a: A) => B) =>
+  (a: OneOrMany<A>): OneOrMany<B> => {
+    if (a.tag === "One") {
+      return one(f(a.value));
+    }
+    return many(NEA.map(f)(a.value));
+  };
 
 type Tramite = {
   tramite_id: string;
   nombre: string;
-}
+};
 
-/* Request ecuadorian tramites */
-const URL = "https://www.gob.ec/api/v1/tramites"
-export const printTramitesEC = (): void => {
+/* Request ecuadorian procedures */
+const URL = "https://www.gob.ec/api/v1/tramites";
+export const printProceduresEC = (): void => {
   pipe(
     TE.tryCatch(
       async () => {
         const resp = await axios.get<Tramite[]>(URL);
         if (resp.status === 200) {
-          return resp.data
+          return resp.data;
         } else {
-          throw new Error(`Failed to get ${URL} with status ${resp.status}`)
+          throw new Error(`Failed to get ${URL} with status ${resp.status}`);
         }
-      }, (x) => console.error((x as Error).message),
+      },
+      (x) => console.error((x as Error).message)
     ),
-    TE.map((x) => x.forEach((y) => console.log(y.nombre))),
-  )
-  ()
-}
+    TE.map((x) => x.forEach((y) => console.log(y.nombre)))
+  )();
+};
 
-printTramitesEC();
+//Another way to do the same printProceduresEC function above
+export const printProceduresEC2 = (): void => {
+  const result = TE.tryCatch(
+    async () => {
+      const resp = await axios.get<Tramite[]>(URL);
+      if (resp.status === 200) {
+        return resp.data;
+      }
+      throw new Error(`Failed to get ${URL} with status ${resp.status}`);
+    },
+    (x) => console.error((x as Error).message)
+  );
+
+  const f = (x: Tramite[]) => x.forEach((y) => console.log(y.nombre));
+  TE.map(f)(result)();
+};
